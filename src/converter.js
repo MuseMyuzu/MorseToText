b0f259a016078010465388b1339544421f86dd70
ミューズ / Muse <91707462+MuseMyuzu@users.noreply.github.com>
2023-01-30 11:14:02 +0900

    duration計算関数を作成

--- src/converter.js
+++ src/converter.js
@@ -1,38 +1,57 @@
 class Converter{
-    static startTime;
-    static endTime;
+    static isPC = false;
+    static pressed = null;
+    static startFrame;
+    static endFrame;
     static duration = [];
     static notPressingDuration;
 
     static initialize(){
-        document.addEventListener("keydown", function(event) {
-            if (event.keyCode === 32) {
-                startTime = Date.now();
-            }
-        });
-    
-        document.addEventListener("keyup", function(event) {
-            if (event.keyCode === 32) {
-                endTime = Date.now();
-                duration.push(endTime - startTime);
-            }
-        });
-    
-        document.addEventListener("keydown", function(event) {
-            if (event.keyCode === 32 && startTime !== null) {
-                notPressingDuration = startTime - endTime;
-                duration.push(notPressingDuration);
-            }
-        });
-    }
+        this.isPC = (window.ontouchstart === undefined) ? true : false;
 
-    //スマホかPCかで変更する
-    static getEventTypeStart(){
-        return (window.ontouchstart === undefined) ? "mousedown" : "touchstart";
-    }
-
-    static getEventTypeEnd(){
-        return (window.ontouchstart === undefined) ? "mouseup" : "touchend";
+        if(this.isPC){
+            //PCの場合（スペースキー）
+            document.addEventListener("keydown", function(event) {
+                if (event.code == "Space") {
+                    this.pressed = true;
+                }
+            });
+        
+            document.addEventListener("keyup", function(event) {
+                if (event.code == "Space") {
+                    this.pressed = false;
+                }
+            });
+        }else{
+            //スマホの場合（タップ）
+            document.addEventListener("touchstart", function(event) {
+                this.pressed = true;
+            });
+        
+            document.addEventListener("touchend", function(event) {
+                this.pressed = false;
+            });
+        }
+        
     }
     
+    /**
+     * スペースを押している時間、離している時間を記録する
+     * @param {int} frame 
+     */
+    static recordDuration(frame){
+        if(this.pressed == true){
+            console.log("b");
+            if(this.startFrame !== null){
+                this.notPressingDuration = this.startFrame - this.endFrame;
+                this.duration.push(this.notPressingDuration);
+            }
+            this.startFrame = frame;
+            console.log(this.duration);
+        }else if(this.pressed == false){
+            console.log("a");
+            this.endFrame = frame;
+            this.duration.push(this.endFrame - this.startFrame);
+        }
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,54 +1,33 @@
 //起動時
 window.addEventListener("load", () => {
-    Main.initialize();
+    initialize();
+    loop();
 })
 
-class Main{
-    static mode;
+let mode;
+let frame;
 
-    static initialize(){
-        //ループを開始
-        Main.loop();
-    }
-
-    static loop(){
-        switch(Main.mode){
-            case "start":
-                break;
-            case "typing":
-                break;
-            case "finish":
-                break;
-        }
-        
-        if(Main.loopFlag){
-            //1/60秒後にもう一度呼び出す
-            requestAnimationFrame(Main.loop);
-        }
-    }
+function initialize(){
+    Converter.initialize();
+    mode = "start";
+    frame = 0;
 }
 
-let startTime;
-let endTime;
-let duration = [];
-let notPressingDuration;
-
-document.addEventListener("keydown", function(event) {
-    if (event.keyCode === 32) {
-        startTime = Date.now();
+function loop(){
+    switch(mode){
+        case "start":
+            mode = "typing";
+            break;
+        case "typing":
+            Converter.recordDuration(frame);
+            break;
+        case "finish":
+            break;
     }
-});
+    frame++;
 
-document.addEventListener("keyup", function(event) {
-    if (event.keyCode === 32) {
-        endTime = Date.now();
-        duration.push(endTime - startTime);
-    }
-});
+    //1/60秒後にもう一度呼び出す
+    requestAnimationFrame(loop);
+}
 
-document.addEventListener("keydown", function(event) {
-    if (event.keyCode === 32 && startTime !== null) {
-        notPressingDuration = startTime - endTime;
-        duration.push(notPressingDuration);
-    }
-});
+
