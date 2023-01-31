28025fb936a74dc259520e59ae5fc37665449c3e
ミューズ / Muse <91707462+MuseMyuzu@users.noreply.github.com>
2023-01-31 21:42:59 +0900

    durationの記録成功

--- src/converter.js
+++ src/converter.js
@@ -1,35 +1,41 @@
 class Converter{
-    static isPC = false;
-    static pressed = null;
-    static startFrame;
-    static endFrame;
-    static duration = [];
-    static notPressingDuration;
+    static isPC; //true: PC, false: phone
+    static pressed; //押されているか
+    static prev_pressed; //前フレームのpressed
+    static startFrame; //押し始めたフレーム
+    static endFrame;   //離したフレーム
+    static duration = []; //押しているとき、押していないときの時間の記録
+    static notPressingDuration; //押していない時間
 
     static initialize(){
+        //PCかスマホか確認
         this.isPC = (window.ontouchstart === undefined) ? true : false;
+        this.pressed = false;
+        this.prev_pressed = false;
+        this.startFrame = 0;
+        this.endFrame = 0;
 
         if(this.isPC){
             //PCの場合（スペースキー）
             document.addEventListener("keydown", function(event) {
                 if (event.code == "Space") {
-                    this.pressed = true;
+                    Converter.pressed = true;
                 }
             });
         
             document.addEventListener("keyup", function(event) {
                 if (event.code == "Space") {
-                    this.pressed = false;
+                    Converter.pressed = false;
                 }
             });
         }else{
             //スマホの場合（タップ）
             document.addEventListener("touchstart", function(event) {
-                this.pressed = true;
+                Converter.pressed = true;
             });
         
             document.addEventListener("touchend", function(event) {
-                this.pressed = false;
+                Converter.pressed = false;
             });
         }
         
@@ -40,18 +46,22 @@ class Converter{
      * @param {int} frame 
      */
     static recordDuration(frame){
-        if(this.pressed == true){
-            console.log("b");
-            if(this.startFrame !== null){
-                this.notPressingDuration = this.startFrame - this.endFrame;
-                this.duration.push(this.notPressingDuration);
-            }
+        //押されたとき
+        if(this.pressed === true && this.prev_pressed === false){
             this.startFrame = frame;
+            this.notPressingDuration = this.startFrame - this.endFrame;
+            this.duration.push(this.notPressingDuration);
             console.log(this.duration);
-        }else if(this.pressed == false){
-            console.log("a");
+        }
+        
+        //離されたとき
+        if(this.pressed === false && this.prev_pressed === true){
             this.endFrame = frame;
             this.duration.push(this.endFrame - this.startFrame);
+            console.log(this.duration);
         }
+
+        //pressedをprev_pressedへ
+        this.prev_pressed = this.pressed;
     }
 }
